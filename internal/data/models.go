4209dfaa8c193ee7615b7a06cfae7e80b1b6ea8f
Niek Kloppenburg <niekkloppenburg@gmail.com>
2024-03-12 13:33:41 +0100

    Added users, authentication and authorisation

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,10 +12,16 @@ var (
 
 type Models struct {
 	Movies MovieModel
+	Permissions PermissionModel
+	Users  UserModel
+	Tokens TokenModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Permissions: PermissionModel{DB: db},
+		Users:   UserModel{DB: db},
+		Tokens: TokenModel{DB: db},
 	}
 }
